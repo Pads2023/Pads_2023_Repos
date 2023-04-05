1e2c7721743113c3e4c9cb79cba5735c51bc6e1e
Pads2023 <129928432+Pads2023@users.noreply.github.com>
2023-04-05 11:19:21 +0530

    import timelibrary 3 times

--- Ebay-Maven/src/test/java/testNg_pack/TestNgClass1.java
+++ Ebay-Maven/src/test/java/testNg_pack/TestNgClass1.java
@@ -1,6 +1,10 @@
 package testNg_pack;
 
 import java.time.Duration;
+import java.time.Duration;
+
+import java.time.Duration;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
